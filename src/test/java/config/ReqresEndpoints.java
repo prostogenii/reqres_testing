490a8279c7fb2e5bb8988e4c8d55e2de0c89c678
prostogenii <andreyzhuravel2004@gmail.com>
2023-01-28 21:50:12 +0200

    Commit

--- src/test/java/config/ReqresEndpoints.java
+++ src/test/java/config/ReqresEndpoints.java
@@ -5,5 +5,6 @@ public interface ReqresEndpoints {
     String SINGLE_USER = "users/{userId}";
     String CREATE_USER = "users";
     String LOG_IN_USER = "login";
+    String LIST_OF_USERS = "users?page=2";
 
 }
--- src/test/java/reqresTests/GpathJSONTests.java
+++ src/test/java/reqresTests/GpathJSONTests.java
@@ -1,10 +1,12 @@
 package reqresTests;
 
+import config.ReqresEndpoints;
 import config.ReqresTestConfig;
 import io.restassured.response.Response;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -12,11 +14,9 @@ import static io.restassured.RestAssured.get;
 
 public class GpathJSONTests extends ReqresTestConfig {
 
-    String ulr = "users?page=2";
-
     @Test
     public void takeOneUserUserFromList(){
-        Response response = get(ulr);
+        Response response = get(ReqresEndpoints.LIST_OF_USERS);
 
         Map<String, ?> oneUserFromList = response.path("data.find{ it.first_name=='Lindsay'}");
 
@@ -26,7 +26,7 @@ public class GpathJSONTests extends ReqresTestConfig {
 
     @Test
     public void assertTheUserNameTest(){
-        Response response = get(ulr);
+        Response response = get(ReqresEndpoints.LIST_OF_USERS);
 
         String user = response.path("data.find{ it.first_name=='Byron'}.last_name");
 
@@ -39,10 +39,24 @@ public class GpathJSONTests extends ReqresTestConfig {
 
     @Test
     public void getAllNamesFrom8IdTo10Id(){
-      Response response = get(ulr);
+      Response response = get(ReqresEndpoints.LIST_OF_USERS);
 
         List<String> usersFrom8To10 = response.path("data.findAll{it.id>=8}.first_name");
 
         System.out.println(usersFrom8To10);
+    }
+
+    @Test
+    public void getUsersUsingArrayListAndMap(){
+
+        String first_name = "Tobias";
+        String last_name = "Edwards";
+
+        Response response = get(ReqresEndpoints.LIST_OF_USERS);
+
+       ArrayList<Map<String, ?>> users = response.path
+               ("data.findAll{it.first_name=='%s'}", first_name);
+
+        System.out.println(users);
     }
 }
